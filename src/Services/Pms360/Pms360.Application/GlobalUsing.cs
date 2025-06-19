a86bcf8a671cd1242c4af3cbeb11b2120759b652
Kaicobad <kaikobad.kaikobad326@gmail.com>
2025-06-19 17:04:52 +0600

    Error solve

--- src/Services/Pms360/Pms360.Application/GlobalUsing.cs
+++ src/Services/Pms360/Pms360.Application/GlobalUsing.cs
@@ -6,3 +6,6 @@ global using Pms360.Domain.Entities;
 global using Pms360.Application.Models.Requests;
 global using Microsoft.EntityFrameworkCore;
 global using FluentValidation.Results;
+global using System.Reflection;
+global using FluentValidation;
+
